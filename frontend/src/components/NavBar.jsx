afcd340a15aeeb54af3a6662368044857d112dab
Alex Chang <135291565+achang1017@users.noreply.github.com>
2025-07-16 12:58:26 -0700

    Update logo.png

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,13 +4,13 @@ import Logo from '../assets/logo.png';
 export default function NavBar() {
   return (
     <header className="fixed color top-4 left-1/2 transform -translate-x-1/2 w-[95%] max-w-6xl z-50">
-      <div className="relative flex flex-col sm:flex-row sm:items-center sm:justify-between bg-white/10 backdrop-blur-lg px-6 py-4 rounded-2xl shadow-lg text-white">
+      <div className="relative flex flex-col sm:flex-row sm:items-center sm:justify-between bg-neutral-800/30 backdrop-blur-lg px-6 py-4 rounded-2xl shadow-lg text-white">
 
         {/* Logo + Brand */}
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-4">
-            <img src={Logo} alt="Jack's Landing Logo" className="w-12 h-12 object-contain" />
-            <span className="font-light text-xl tracking-wider">JACK'S LANDING</span>
+            <img src={Logo} alt="Jack's Landing Logo" className="w-15 h-15 object-contain" />
+            <span className="font-light text-xl tracking-wider">Jack's Landing</span>
           </div>
 
           {/* Hamburger icon (mobile only) */}
@@ -31,9 +31,9 @@ export default function NavBar() {
         >
           <ul className="flex flex-col sm:flex-row gap-y-4 sm:gap-x-10 text-base font-light tracking-wide">
             <li><Link to="/" className="hover:text-white/80 transition-colors">Home</Link></li>
-            <li><Link to="/activities" className="hover:text-white/80 transition-colors">Activities</Link></li>
-            <li><Link to="/availability" className="hover:text-white/80 transition-colors">Availability</Link></li>
-            <li><Link to="/contact" className="hover:text-white/80 transition-colors">Contact Us</Link></li>
+            <li><Link to="/Activities" className="hover:text-white/80 transition-colors">Activities</Link></li>
+            <li><Link to="/Availability" className="hover:text-white/80 transition-colors">Availability</Link></li>
+            <li><Link to="/Contact" className="hover:text-white/80 transition-colors">Contact Us</Link></li>
           </ul>
         </nav>
       </div>
